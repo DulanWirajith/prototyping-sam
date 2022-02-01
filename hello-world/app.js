f14176846a4e8441d9fd6e4be7f3b1a26c875577
dulan-ca <dulan@cabbageapps.com>
2022-02-01 11:05:45 +0530

    basics in sam project

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world',
+                message: `hello ${event['first-name']} ${event['last-name']}`,
                 // location: ret.data.trim()
             })
         }
